700a9fb91ecb171ef22f9c2f5dee5ea775c66a22
rkirk430 <r.kirk430@gmail.com>
2022-07-07 15:59:09 -0400

    Nav Open and Close Functionality:

--- main.js
+++ main.js
@@ -5,4 +5,13 @@ const menu = document.querySelector(".nav__items");
 //Add Event Listener for a click to access the Nav Bar Menu
 menuBtn.addEventListener("click", () => {
     menu.style.display = 'block';
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'inline-block';
+});
+
+//Add Event Listener for a click to close the Nav Bar Menu
+closeBtn.addEventListener("click", () => {
+    menu.style.display = 'none';  //Hides Open Nav Items after the click to close them
+    menuBtn.style.display = 'inline-block'; //Displays Nav dropdown picture link after clicking 
+    closeBtn.style.display = 'none'; // Removes close button from view
 })
